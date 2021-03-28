2996e2496e8df73755a8a20e28d6daee319ed089
KT <au637855@uni.au.dk>
2021-03-28 16:07:02 +0200

    schedule basically done (TO DO: make prettier)

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', setup, false);
+
+function setup() {
+    addSchedule();
+}
+
+function addSchedule() {
     let calendar = document.getElementById("calendar");
     let currentDay = calendar.children[new Date().getDay()];
     currentDay.className = "calendar-table-row-header-today";
-    for (let i = 0; i < 8*24; i++) {
+    
+    for (let i = 0; i < 8*(17-8); i++) {
         var element = document.createElement("div");
         if (i % 8 == 0) {
-            let time = i / 8;
+            let time = i / 8 + 8;
             element.innerHTML = ("0" + time).slice(-2) + " - " + ("0" + (time+1)).slice(-2);
             element.classList.add("calendar-table-header");
         } else {
@@ -14,7 +21,7 @@ document.addEventListener('DOMContentLoaded', function() {
         }
         calendar.appendChild(element);
     }
-}, false);
+}
 
 function previousWeek() {
     alert("Sidste uge :D");
@@ -22,4 +29,15 @@ function previousWeek() {
 
 function nextWeek() {
     alert("Næste uge :D");
+}
+
+
+// Schedule
+
+function setActiveButton(button) {
+    var c = button.parentElement.children;
+    for (let i = 0; i < c.length; i++) {
+      c[i].classList.remove("active-button");
+    }
+    button.classList.add("active-button");
 }
