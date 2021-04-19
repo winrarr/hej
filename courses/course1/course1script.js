a55f6efb4eb3d9791781295276e9b97d48ade3c2
Morten <lerazzomh@gmail.com>
2021-04-19 14:45:57 +0200

    Merge conflict manually solved

--- courses/course1/course1script.js
+++ courses/course1/course1script.js
@@ -5,31 +5,46 @@ function setup() {
 }
 
 function displayCoursePlan() {
+    console.log("hej");
     let coursecontent = document.getElementById("content-table");
     
     for (let row = 0; row < 20; row++) { //this is rows! change later!
         let trow = document.createElement('tr');
+
         //Day
         let day = document.createElement('td');
         day.innerHTML = row;
+        trow.appendChild(day);
+
         //Lecture
         let lecture = document.createElement('td');
         lecture.innerHTML = row;
+        trow.appendChild(lecture);
+
         //Literature
         let literature = document.createElement('td');
         literature.innerHTML = row;
+        trow.appendChild(literature);
+
         //Slides
         let slides = document.createElement('td');
         slides.innerHTML = row;
+        trow.appendChild(slides);
+
         //Videos
         let videos = document.createElement('td');
         videos.innerHTML = row;
+        trow.appendChild(videos);
+
         //Exercises
         let exercises = document.createElement('td');
         exercises.innerHTML = row;
+        trow.appendChild(exercises);
+
         //Assignments
         let assignments = document.createElement('td');
         assignments.innerHTML = "none!";
+        trow.appendChild(assignments);
     
         coursecontent.appendChild(trow);
     }
