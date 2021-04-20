6dc4cae7c030c8a43e6e9446c012e6e28b8e524f
Morten <lerazzomh@gmail.com>
2021-04-20 13:51:54 +0200

    All pages on other info now contain placeholder information

--- courses/course1/course1script.js
+++ courses/course1/course1script.js
@@ -4,6 +4,7 @@ var container;
 
 function setup() {
     container = document.getElementById("content-container");
+    displayCoursePlan();
 }
 
 function reset() {
@@ -12,7 +13,18 @@ function reset() {
 
 function displayCoursePlan() {
     reset();
-    container.innerHTML = "<table id='content-table'><tr><th>Day</th><th>Lecture</th><th>Literature</th><th>Slides</th><th>Videos</th><th>Exercises</th><th>Assignments</th></tr> </table>";
+    container.innerHTML = `
+    <table id='content-table'>
+        <tr>
+            <th>Day</th>
+            <th>Lecture</th>
+            <th>Literature</th>
+            <th>Slides</th>
+            <th>Videos</th>
+            <th>Exercises</th>
+            <th>Assignments</th>
+        </tr>
+    </table>`;
     let coursecontent = document.getElementById("content-table");
     
     for (let row = 0; row < 20; row++) { //this is rows! change later!
@@ -61,9 +73,73 @@ function displayCoursePlan() {
 
 function displayOtherInfo(){
     reset();
-    container.innerHTML = "INFO INFO";
+    container.innerHTML = `
+    <div id="other-info">
+        <button onclick="otherInfoDisplayIntroduction()">Introduction to Course</button>
+        <button onclick="otherInfoDisplayTools()">Tools</button>
+        <button onclick="otherInfoDisplayGroups()">Groups</button>
+        <button onclick="otherInfoDisplayExam()">Exam</button>
+        <button onclick="otherInfoDisplayContacts()">Contacts</button>
+        <button onclick="otherInfoDisplayEvaluation()">Evaluation</button>
+    </div>`;
 }
 
+function otherInfoDisplayIntroduction() {
+    reset();
+    container.innerHTML = `
+    <h3>Welcome</h3>
+    This course has three major parts and during each part, we will introduce you to one of the major components of modern computers. In the first part, we will focus on computer architecture where we will discuss the existing levels in the architecture of modern computers, as well as topics such as microprograms and assembly language. In the second part, we will focus on operating systems and we will go over a brief introduction of some major components of operating systems including processes, system calls, virtual memory and other important topics. In today's well-connected world, it is unthinkable to not be connected to internet which brings us to the final major topic: computer networks. Here, we will cover some of major components including different networking layers, sockets, TCP/PC and other topics.
+    `;
+}
+function otherInfoDisplayTools(){
+    reset();
+    container.innerHTML = `
+    <h3>Tools</h3>
+    The tools in this course will be introduced during TA exercises. This page will be updated when they are needed.
+    `;
+} 
+function otherInfoDisplayGroups() {
+    reset();
+    container.innerHTML = `
+    <h3>Groups</h3>
+    Here you can sign up for groups
+    <button onclick="alert('You have signed up for a group')">Sign Up for a random group</button>
+    `;
 
+}
+function otherInfoDisplayExam() {
+    reset();
+    container.innerHTML = `
+    <h3>Exam</h3>
+    <p>The exam will be extremely hard and you will all fail.</p>
+    <p>That is just how it is.</p>
+    <p>There will be 200 questions and you have to get 194 right to pass.
+    You have a generous timeframe of 16 minutes.</p>
+    `;
 
+}
+function otherInfoDisplayContacts() {
+    reset();
+    container.innerHTML = `
+    <h3>Contacts</h3>
+    <p>The lecturer of this course is Johnny Johnson. Contact him at office 204 or by email: <a href="mailto: john@john.com">john@john.com</a></p>
+    <p>The TA's are Sabrina Jade and Kevin Koda, but dont contact them, they dont want that</p>
+    `;
+}
+function otherInfoDisplayEvaluation() {
+    reset();
+    container.innerHTML = `
+    <h3>Evaluation</h3>
+    What do you rate the course?<br>
+    <input type="radio" name="rating">1<br>
+    <input type="radio" name="rating">2<br>
+    <input type="radio" name="rating">3<br>
+    <input type="radio" name="rating">4<br>
+    <input type="radio" name="rating">5<br>
 
+    What could we do better?<br>
+    <input type="text"><br>
+
+    <input type="submit">
+    `;
+}
