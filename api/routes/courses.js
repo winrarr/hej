59788270681a01553e0b1852c98dc36012d3f77d
KT <au637855@uni.au.dk>
2021-05-07 18:33:30 +0200

    made /api/coures return all queries when no query is specified

--- api/routes/courses.js
+++ api/routes/courses.js
@@ -1,20 +1,25 @@
 const routes = require('express').Router()
 const db = require("../database/database.js")
 
-routes.get("/all", async (req, res) => {
-    const sql = "select * from course"
-    db.all(sql, (err, rows) => res.send(rows))
-})
 
 routes.get("", async (req, res) => {
     const abbreviation = req.query.abbr
 
-    getCourseFromAbbreviation(abbreviation, res)
+    if (abbreviation) {
+        getCourseFromAbbr(abbreviation, res)
+    } else {
+        getAllCourses(res)
+    }
 })
 
-function getCourseFromAbbreviation(abbreviation, res) {
+function getCourseFromAbbr(abbreviation, res) {
     const sql = "select * from course where upper(abbreviation)=upper(?)"
     db.all(sql, [abbreviation], (err, rows) => res.send(rows))
+}
+
+function getAllCourses(res) {
+    const sql = "select * from course"
+    db.all(sql, (err, rows) => res.send(rows))
 }
 
 
